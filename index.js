ba290d837fbc393f6bbfb93ed6f17bdceb4a6d3d
angie <angieangerilli@gmail.com>
2019-02-12 17:00:16 -0500

    switch statement

---
+++ index.js
@@ -0,0 +1,46 @@
+var points = 5;
+switch (points) {
+case 10:
+console.log("You recieved 100%, this is a passing grade!");
+break;
+
+case 9:
+console.log("You recieved 90%, this is a passing grade!");
+break;
+
+case 8:
+console.log("You recieved 80%, this is a passing grade!");
+break;
+
+case 7:
+console.log("You recieved 70%, this is a passing grade!");
+break;
+
+case 6:
+console.log("You recived a 60%, this is a passing grade!");
+break;
+
+case 5:
+console.log("You recieved a 50%, this is not a passing grade.");
+break;
+
+case 4:
+console.log("You recieved a 40%, this is not a passing grade.");
+break;
+
+case 3:
+console.log("You recieved a 30%, this is not a passing grade.");
+break;
+
+case 2:
+console.log("You recieved a 20%, this is not a passing grade.");
+break;
+
+case 1:
+console.log("You recieved a 10%, this is not a passing grade.");
+break;
+
+default:
+console.log("error");
+break;
+}
